0a5ef3c02eddc080a024ce36af2c30ec2bfceb2c
kharij_000 <kharijamil@gmail.com>
2025-03-05 20:46:20 -0500

    Add code crafters

---
+++ RUBY/test.rb
@@ -0,0 +1,4 @@
+# Welcome to the Build your own Shell challenge!
+#A shell is a command-line interface that executes commands and manages processes. In this challenge, you'll build your own POSIX compliant shell that's capable of interpreting shell commands, running external programs and builtin commands like cd, pwd, echo and more.
+#Along the way, you'll learn about shell command parsing, REPLs, builtin commands, and more.
+
